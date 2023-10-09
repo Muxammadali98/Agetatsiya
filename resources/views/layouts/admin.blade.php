549a466e075d39b2c1c571ad4e13efda7683c032
Muxammadali98 <turgunaliyevmuhammadali@gmail.com>
2023-10-09 17:18:55 +0500

    remoboot

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,7 +23,7 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <!-- Add Bootstrap CSS and JavaScript to the head of your HTML file -->
-     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+{{--     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">--}}
 
 
     @livewireStyles
