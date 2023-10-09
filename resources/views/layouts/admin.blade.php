69c0e0e9e3284ab4c63647504843a83351930a5e
Muxammadali98 <turgunaliyevmuhammadali@gmail.com>
2023-10-09 17:18:09 +0500

    addnotifi5

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -127,7 +127,7 @@
                     <div class="header-right">
 
 
-                        <livewire:notifikation />
+                        <livevare:notifikation/>
 
                         <!-- profile start -->
                         <div class="profile-box ml-15">
